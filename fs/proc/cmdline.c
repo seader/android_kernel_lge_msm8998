610b457dedc46b97df6183830490b8be6d3cecac
SGCMarkus <markusornik@gmail.com>
2019-04-12 13:06:17 +0200

    system as root
    Change-Id: I60593433b09bcf15479ec0b8c47461d173ec0ead

--- fs/proc/cmdline.c
+++ fs/proc/cmdline.c
@@ -2,9 +2,35 @@
 #include <linux/init.h>
 #include <linux/proc_fs.h>
 #include <linux/seq_file.h>
+#include <soc/qcom/lge/board_lge.h>
+#include <asm/setup.h>
+#include <linux/slab.h>
+
+static char updated_command_line[COMMAND_LINE_SIZE];
+static void proc_cmdline_set(char *name, char *value)
+{
+	char *flag_pos, *flag_after;
+	char *flag_pos_str = kmalloc(sizeof(char), COMMAND_LINE_SIZE);
+	scnprintf(flag_pos_str, COMMAND_LINE_SIZE, "%s=", name);
+	flag_pos = strstr(updated_command_line, flag_pos_str);
+	if (flag_pos) {
+		flag_after = strchr(flag_pos, ' ');
+		if (!flag_after)
+			flag_after = "";
+		scnprintf(updated_command_line, COMMAND_LINE_SIZE, "%.*s%s=%s%s",
+				(int)(flag_pos - updated_command_line),
+				updated_command_line, name, value, flag_after);
+	} else {
+		// flag was found, insert it
+		scnprintf(updated_command_line, COMMAND_LINE_SIZE, "%s %s=%s", updated_command_line, name, value);
+	}
+}
 
 static int cmdline_proc_show(struct seq_file *m, void *v)
 {
+	proc_cmdline_set("androidboot.verifiedbootstate", "green");
+
+
 	seq_printf(m, "%s\n", saved_command_line);
 	return 0;
 }
